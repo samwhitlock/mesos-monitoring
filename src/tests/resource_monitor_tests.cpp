fd03f152e8d938c3b378788b51223c7f1123f447
Alex Degtiar <alex.degtiar@gmail.com>
2012-02-22 20:03:50 -0800

    Updating tests for Futures and making them build

--- src/tests/resource_monitor_tests.cpp
+++ src/tests/resource_monitor_tests.cpp
@@ -20,11 +20,12 @@
 #include <gmock/gmock.h>
 
 #include <process/process.hpp>
+#include <process/future.hpp>
 
 #include "common/try.hpp"
 #include "common/resources.hpp"
 #include "monitoring/resource_collector.hpp"
-#include "monitoring/resource_monitor.hpp"
+#include "slave/resource_monitor.hpp"
 
 /* Tests for resource monitor
  * - just one test to make sure that it pushes through state correctly
@@ -33,9 +34,12 @@
 using namespace mesos;
 using namespace mesos::internal;
 using namespace mesos::internal::monitoring;
+using namespace mesos::internal::slave;
 
-using testing::Return;
 using process::Clock;
+using process::Future;
+using std::string;
+using testing::Return;
 
 class MockCollector : public ResourceCollector
 {
@@ -43,35 +47,54 @@ public:
   // TODO(sam): Fix/confirm change.
   MOCK_METHOD0(getMemoryUsage, Try<double>());
   MOCK_METHOD0(getCpuUsage, Try<Rate>());
+  MOCK_METHOD0(collectUsage, void());
 };
 
 TEST(ResourceMonitorTest, MonitorsCorrectly)
 {
-  MockCollector* mc = new MockCollector();
+  MockCollector* mock_collector = new MockCollector();
 
   // TODO(sam) put call requirements here!
+  // Set the memory usage the collector will return.
   double memoryUsage = 123456789.0;
-  EXPECT_CALL(*mc, getMemoryUsage())
+  EXPECT_CALL(*mock_collector, getMemoryUsage())
     .Times(1)
     .WillOnce(Return(Try<double>::some(memoryUsage)));
 
+  // Set the cpu usage the collector will return.
   double duration = 13579.0, difference = 2468.0;
-  EXPECT_CALL(*mc, getCpuUsage())
+  EXPECT_CALL(*mock_collector, getCpuUsage())
     .Times(1)
     .WillOnce(Return(Try<Rate>::some(Rate(duration, difference))));
 
-  ResourceMonitor rm(mc);
+  ResourceMonitor mocked_monitor(mock_collector);
 
-  Try<UsageReport> tur = rm.collectUsage();
+  // Create fake IDs.
+  FrameworkID framework_id;
+  framework_id.set_value("framework_id1");
+  ExecutorID executor_id;
+  executor_id.set_value("executor_id1");
 
-  ASSERT_TRUE(tur.isSome());
+  // Collect the Usage.
+  Future<UsageMessage> usage_msg_future = mocked_monitor.collectUsage(
+      framework_id, executor_id);
+
+  // Setting a timeout in case it gets stuck.
+  usage_msg_future.await(5);
+
+  ASSERT_TRUE(usage_msg_future.isReady());
   
-  UsageReport ur = tur.get();
-  Resources r = ur.resources;
+  UsageMessage usage_msg = usage_msg_future.get();
 
-  EXPECT_EQ(duration, ur.duration);
-  EXPECT_FALSE(ur.timestamp > Clock::now());//To fix roundoff errors
+  // Make sure the returned UsageMessage matches the expected values.
+  EXPECT_EQ(duration, usage_msg.duration());
+  EXPECT_FALSE(usage_msg.timestamp() > Clock::now()); //To fix roundoff errors
+  
+  // TODO(adegtiar or sam): make this the correct call.
+  //EXPECT_EQ(framework_id, usage_msg.framework_id());
+  //EXPECT_EQ(executor_id, usage_msg.executor_id());
 
-  EXPECT_EQ(memoryUsage, r.get("mem_usage", Resource::Scalar()).value());
-  EXPECT_EQ(difference, r.get("cpu_usage", Resource::Scalar()).value());
+  Resources usage = usage_msg.resources();
+  EXPECT_EQ(memoryUsage, usage.get("mem_usage", Value::Scalar()).value());
+  EXPECT_EQ(difference, usage.get("cpu_usage", Value::Scalar()).value());
 }
